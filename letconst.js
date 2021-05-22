67451ee2532862fbd04a85b6745835ec89681f13
googlermridul <goooglermridul@gmail.com>
2021-05-23 05:31:57 +0600

    first commit

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const wifi = "pushpita";
+// wifi = "shahrin"
+console.log(wifi);
+
+
+const numbers = [12, 23];
+numbers[0] = 32;
+numbers.push(21)
+console.log(numbers);
+
+
+let patientName = "rahimer dokan";
+patientName = "fatema enterprise"
+console.log(patientName);
+
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+   sum = sum + i;
+   console.log(sum);
+}
+console.log(sum);
